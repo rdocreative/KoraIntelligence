eb19aae68bdc06de11bb387c7ff7b49788ef6f82
[dyad] <playgrooz368@gmail.com>
2026-02-19 14:15:19 -0300

    [dyad] Adicionando um degradê radial vermelho sutil no centro do fundo. - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -43,6 +43,9 @@ const App = () => (
                   
                   {/* Background Layers */}
                   <ParticleBackground />
+                  
+                  {/* Central Red Glow Gradient */}
+                  <div className="fixed inset-0 pointer-events-none bg-[radial-gradient(circle_at_50%_50%,rgba(220,38,38,0.08),transparent_70%)]" />
 
                   <div className="relative z-10 flex flex-col min-h-screen">
                     <TopBar />
