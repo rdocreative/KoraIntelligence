8c6fb4c110ba44c60e650d5cbc00910f75f0d616
[dyad] <playgrooz368@gmail.com>
2026-02-23 17:27:00 -0300

    [dyad] Criação da aba de gráficos no dashboard de hábitos com Recharts. - wrote 4 file(s), added recharts package(s)

--- src/pages/Habits.tsx
+++ src/pages/Habits.tsx
@@ -894,7 +894,7 @@ const HabitsPage = () => {
         </div>
       ) : (
         <div className="mt-8">
-          <DashboardOverview />
+          <DashboardOverview habits={habits} />
         </div>
       )}
 
