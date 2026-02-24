ed0b51fc49226cfb4c8a1af97b6f05d2922d97d1
[dyad] <playgrooz368@gmail.com>
2026-02-24 20:26:29 -0300

    [dyad] Alinhado padding lateral do conteúdo com a TopBar no App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const ProtectedRoute = () => {
       <SideNav />
       <div className="flex-1 flex flex-col min-h-screen pl-[85px]">
         <TopBar />
-        <main className="flex-1 p-5 max-w-[100rem] mx-auto w-full overflow-visible">
+        <main className="flex-1 px-8 py-5 max-w-[100rem] mx-auto w-full overflow-visible">
           <Outlet />
         </main>
       </div>
