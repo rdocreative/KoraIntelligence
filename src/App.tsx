f404b9fc0822aaa8d75baefb29df872e9593645d
[dyad] <playgrooz368@gmail.com>
2026-02-24 20:31:35 -0300

    [dyad] Alinhamento final de padding entre Main e TopBar - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ const ProtectedRoute = () => {
       <SideNav />
       <div className="flex-1 flex flex-col min-h-screen pl-[85px]">
         <TopBar />
-        <main className="flex-1 px-8 py-5 w-full overflow-visible">
+        <main className="flex-1 px-5 py-5 w-full max-w-[100rem] mx-auto overflow-visible">
           <Outlet />
         </main>
       </div>
